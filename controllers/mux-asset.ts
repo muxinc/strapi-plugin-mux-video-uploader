0bdc5bfab77b6cdfa5c7f50b43ddd8ec236f01a2
Erik Peña <erikpena@users.noreply.github.com>
2020-09-24 16:08:56 -0700

    Change Export types - Change to es6 styles exports for better compatibility

--- controllers/mux-asset.ts
+++ controllers/mux-asset.ts
@@ -2,76 +2,83 @@
 import { sanitizeEntity, parseMultipartData } from 'strapi-utils';
 import { Context } from 'koa';
 
-module.exports = {
-
-  index: async (ctx:Context) => {
-    ctx.send({
-      message: 'ok'
-    });
-  },
-
-  find: async (ctx:Context) => {
-    let entities;
-
-    if (ctx.query._q) {
-      entities = await strapi.entityService.search({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
-    } else {
-      entities = await strapi.entityService.find({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
-    }
-
-    return entities.map((entity:any) => sanitizeEntity(entity, {model: { options: {}, attributes:[]}}));
-  },
-
-  findOne: async (ctx:Context) => {
-    const { id } = ctx.params;
-
-    const entity = await strapi.entityService.findOne({ id }, {model: 'plugins::mux.mux-asset'});
-
-    return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
-  },
-
-  count: (ctx:Context) => {
-    if (ctx.query._q) {
-      return strapi.entityService.countSearch({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
-    }
-    return strapi.entityService.count({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
-  },
-
-  create: async (ctx:Context) => {
-    let entity;
-    
-    if (ctx.is('multipart')) {
-      const { data, files } = parseMultipartData(ctx);
-
-      entity = await strapi.entityService.create({ data, files }, {model: 'plugins::mux.mux-asset'});
-    } else {
-      entity = await strapi.entityService.create({ data: ctx.request.body }, {model: 'plugins::mux.mux-asset'});
-    }
-
-    return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
-  },
-
-  update: async (ctx:Context) => {
-    const { id } = ctx.params;
-
-    let entity;
-
-    if (ctx.is('multipart')) {
-      const { data, files } = parseMultipartData(ctx);
-
-      entity = await strapi.entityService.update({ params: { id }, data, files }, {model: 'plugins::mux.mux-asset'});
-    } else {
-      entity = await strapi.entityService.update({ params: { id }, data: ctx.request.body }, {model: 'plugins::mux.mux-asset'});
-    }
-
-    return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
-  },
-
-  delete: async (ctx:Context) => {
-    const { id } = ctx.params;
-
-    const entity = await strapi.entityService.delete({ params: { id } }, {model: 'plugins::mux.mux-asset'});
-
-    return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
-  },
+const index = async (ctx:Context) => {
+  ctx.send({
+    message: 'ok'
+  });
 };
+
+const find = async (ctx:Context) => {
+  let entities;
+
+  if (ctx.query._q) {
+    entities = await strapi.entityService.search({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
+  } else {
+    entities = await strapi.entityService.find({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
+  }
+
+  return entities.map((entity:any) => sanitizeEntity(entity, {model: { options: {}, attributes:[]}}));
+};
+
+const findOne = async (ctx:Context) => {
+  const { id } = ctx.params;
+
+  const entity = await strapi.entityService.findOne({ id }, {model: 'plugins::mux.mux-asset'});
+
+  return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
+};
+
+const count = (ctx:Context) => {
+  if (ctx.query._q) {
+    return strapi.entityService.countSearch({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
+  }
+  
+  return strapi.entityService.count({params: ctx.query}, {model: 'plugins::mux.mux-asset'});
+};
+
+const create = async (ctx:Context) => {
+  let entity;
+  
+  if (ctx.is('multipart')) {
+    const { data, files } = parseMultipartData(ctx);
+
+    entity = await strapi.entityService.create({ data, files }, {model: 'plugins::mux.mux-asset'});
+  } else {
+    entity = await strapi.entityService.create({ data: ctx.request.body }, {model: 'plugins::mux.mux-asset'});
+  }
+
+  return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
+};
+
+const update = async (ctx:Context) => {
+  const { id } = ctx.params;
+
+  let entity;
+
+  if (ctx.is('multipart')) {
+    const { data, files } = parseMultipartData(ctx);
+
+    entity = await strapi.entityService.update({ params: { id }, data, files }, {model: 'plugins::mux.mux-asset'});
+  } else {
+    entity = await strapi.entityService.update({ params: { id }, data: ctx.request.body }, {model: 'plugins::mux.mux-asset'});
+  }
+
+  return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
+};
+
+const del = async (ctx:Context) => {
+  const { id } = ctx.params;
+
+  const entity = await strapi.entityService.delete({ params: { id } }, {model: 'plugins::mux.mux-asset'});
+
+  return sanitizeEntity(entity, {model: { options: {}, attributes:[]}});
+};
+
+export {
+  index,
+  find,
+  findOne,
+  create,
+  update,
+  del
+}
--- controllers/mux-settings.ts
+++ controllers/mux-settings.ts
@@ -3,35 +3,39 @@ import { Context } from 'koa';
 
 import { setConfig, getConfig, deleteConfig } from "../services/mux";
 
-module.exports = {
-  isConfiged: async (ctx:Context) => {
-    const config = await getConfig('general');
+const isConfiged = async (ctx:Context) => {
+  const config = await getConfig('general');
 
-    if(!config.access_token) ctx.send(false);
-    else if(!config.secret_key) ctx.send(false);
-    else if(!config.webhook_signing_secret) ctx.send(false);
-    else ctx.send(true);
-  },
+  if(!config.access_token) ctx.send(false);
+  else if(!config.secret_key) ctx.send(false);
+  else if(!config.webhook_signing_secret) ctx.send(false);
+  else ctx.send(true);
+};
 
-  clearConfig: async (ctx:Context) => {
-    await deleteConfig('general');
+const clearConfig = async (ctx:Context) => {
+  await deleteConfig('general');
 
-    ctx.send({ message: 'ok' });
-  },
+  ctx.send({ message: 'ok' });
+};
 
-  saveConfig: async (ctx:Context) => {
-    const config = pick(ctx.request.body, 'access_token', 'secret_key', 'webhook_signing_secret');
+const saveConfig = async (ctx:Context) => {
+  const config = pick(ctx.request.body, 'access_token', 'secret_key', 'webhook_signing_secret');
 
-    let successful = false;
+  let successful = false;
 
-    if(!isEmpty(config)) {
-      successful = await setConfig('general', config);
-    }
-
-    if(isEmpty(config) || !successful) {
-      ctx.send({ message: `Nothing to update` });
-    } else {
-      ctx.send({ message: `Updated ${Object.keys(config).join(', ')}` });
-    }
+  if(!isEmpty(config)) {
+    successful = await setConfig('general', config);
   }
+
+  if(isEmpty(config) || !successful) {
+    ctx.send({ message: `Nothing to update` });
+  } else {
+    ctx.send({ message: `Updated ${Object.keys(config).join(', ')}` });
+  }
+};
+
+export {
+  isConfiged,
+  clearConfig,
+  saveConfig
 };
