b6e64f044e65e2cd2efa8046cfb06f19b2ff1fdd
Erik Peña <erikpena@users.noreply.github.com>
2022-06-20 21:21:14 -0700

    Enhance rate limit process - Added new rate-limit module with a forEachRateLimit function

--- server/utils/sync.ts
+++ server/utils/sync.ts
@@ -1,12 +1,9 @@
 import { getService } from '.';
 import { MuxAsset } from '../../types';
 import pluginId from './../../admin/src/pluginId';
+import { forEachRateLimit } from './rate-limit';
 
 const model = `plugin::${pluginId}.mux-asset`;
-
-function sleep(ms:number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 const sync = async () => {
   const filters = {
@@ -31,44 +28,36 @@ const sync = async () => {
 
   strapi.log.info(`[mux-video-uploader] Found ${assets.length} to be sync'd`);
 
-  // Respect Mux's 5 requests per second API limit for reading
-  const chunkSize = 5;
-  for (let i = 0; i < assets.length; i += chunkSize) {
-    const chunk = assets.slice(i, i + chunkSize);
-    
-    chunk.forEach(async (a:MuxAsset) => {
-      let asset;
-      if (a.asset_id) {
-        asset = await getService('mux').getAssetById(a.asset_id);
-      } else if (a.upload_id) {
-        asset = await getService('mux').getAssetByUploadId(a.upload_id);
-      } else {
-        strapi.log.warn('Unresolvable asset for sync:', JSON.stringify(a));
-        return;
+  const predicate = async (a:MuxAsset) => {
+    let asset;
+    if (a.asset_id) {
+      asset = await getService('mux').getAssetById(a.asset_id);
+    } else if (a.upload_id) {
+      asset = await getService('mux').getAssetByUploadId(a.upload_id);
+    } else {
+      strapi.log.warn('Unresolvable asset for sync:', JSON.stringify(a));
+      return;
+    }
+
+    const playback_id = asset.playback_ids?.find(playback_id => playback_id.policy === 'public')?.id
+
+    const params = {
+      data: {
+        asset_id: asset.id,
+        playback_id,
+        duration: asset.duration,
+        aspect_ratio: asset.aspect_ratio,
+        isReady: true
       }
+    };
 
-      const playback_id = asset.playback_ids?.find(playback_id => playback_id.policy === 'public')?.id
+    await strapi.entityService.update(model, a.id, params);
 
-      const params = {
-        data: {
-          asset_id: asset.id,
-          playback_id,
-          duration: asset.duration,
-          aspect_ratio: asset.aspect_ratio,
-          isReady: true
-        }
-      };
+    strapi.log.info(`[mux-video-uploader] Updated mux-asset with id: ${a.id}`);
+  };
 
-      await strapi.entityService.update(model, a.id, params);
-
-      strapi.log.info(`[mux-video-uploader] Updated mux-asset with id: ${a.id}`);
-    });
-
-    // I'm sorry for these sins
-    await sleep(1000);
-  }
+  // Respect Mux's 5 requests per second API limit for reading
+  forEachRateLimit<MuxAsset>(assets, predicate, { count: 5 });
 };
 
-export {
-  sync
-};
+export { sync };
