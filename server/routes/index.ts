cd65d1046e066103484e58d64c41fb1ac542dbee
Erik Peña <erikpena@users.noreply.github.com>
2022-11-11 21:45:23 -0600

    Split admin and user operations - Allow distinct permissions for users and admins - User permissions are reduced down to read operations

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,131 +1,15 @@
-const routes = [
-  {
-    "method": "POST",
-    "path": "/mux-video-uploader/submitDirectUpload",
-    "handler": "mux.submitDirectUpload",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
+import adminRoutes from './admin';
+import clientRoutes from './client';
+
+const routes = {
+  "admin": {
+    type: "admin",
+    routes: adminRoutes
   },
-  {
-    "method": "POST",
-    "path": "/mux-video-uploader/submitRemoteUpload",
-    "handler": "mux.submitRemoteUpload",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "POST",
-    "path": "/mux-video-uploader/deleteMuxAsset",
-    "handler": "mux.deleteMuxAsset",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "POST",
-    "path": "/mux-video-uploader/webhook-handler",
-    "handler": "mux.muxWebhookHandler",
-    "config": {
-      "auth": false,
-      "prefix": false
-    }
-  },
-  {
-    "method": "GET",
-    "path": "/mux-video-uploader/thumbnail/:playbackId",
-    "handler": "mux.thumbnail",
-    "config": {
-      "auth": false,
-      "prefix": false
-    }
-  },
-  {
-    "method": "GET",
-    "path": "/mux-video-uploader/mux-asset",
-    "handler": "mux-asset.find",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "GET",
-    "path": "/mux-video-uploader/mux-asset/count",
-    "handler": "mux-asset.count",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "GET",
-    "path": "/mux-video-uploader/mux-asset/:id",
-    "handler": "mux-asset.findOne",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "POST",
-    "path": "/mux-video-uploader/mux-asset",
-    "handler": "mux-asset.create",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "PUT",
-    "path": "/mux-video-uploader/mux-asset/:id",
-    "handler": "mux-asset.update",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "DELETE",
-    "path": "/mux-video-uploader/mux-asset/:id",
-    "handler": "mux-asset.del",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-    
-  {
-    "method": "GET",
-    "path": "/mux-video-uploader/mux-settings",
-    "handler": "mux-settings.isConfiged",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "POST",
-    "path": "/mux-video-uploader/mux-settings",
-    "handler": "mux-settings.saveConfig",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
-  },
-  {
-    "method": "DELETE",
-    "path": "/mux-video-uploader/mux-settings",
-    "handler": "mux-settings.clearConfig",
-    "config": {
-      "policies": [],
-      "prefix": false
-    }
+  "content-api": {
+    type: "content-api",
+    routes: clientRoutes
   }
-];
+};
 
 export default routes;
