e358da8f64d31846df4f6c9d100d903049d287df
Erik Peña <erikpena@users.noreply.github.com>
2022-11-19 16:49:04 -0800

    Added pre-commit hook - Using husky, execute pretty-quick on files staged for commit - Updated the README_INTERNAL.md with dev environment set up steps - Ran initial prettier (was surpised how few file were updated 😍)

--- server/controllers/mux.ts
+++ server/controllers/mux.ts
@@ -14,20 +14,19 @@ const { Webhooks } = Mux;
 
 const model = `plugin::${pluginId}.mux-asset`;
 
-const resolveMuxAssets = async (filters:MuxAssetFilter) => {
+const resolveMuxAssets = async (filters: MuxAssetFilter) => {
   const params = { filters };
-  
+
   const muxAssets = await strapi.entityService.findMany(model, params);
 
   if (muxAssets.length === 1) {
     return muxAssets[0];
-  }
-  else {
+  } else {
     throw new Error('Unable to resolve mux-asset');
   }
 };
 
-const processWebhookEvent = async (webhookEvent:any) => {
+const processWebhookEvent = async (webhookEvent: any) => {
   const { type, data } = webhookEvent;
 
   switch (type) {
@@ -36,8 +35,8 @@ const processWebhookEvent = async (webhookEvent:any) => {
       return [
         muxAsset.id,
         {
-          data: { asset_id: data.asset_id }
-        }
+          data: { asset_id: data.asset_id },
+        },
       ];
     }
     case 'video.asset.ready': {
@@ -49,9 +48,9 @@ const processWebhookEvent = async (webhookEvent:any) => {
             playback_id: data.playback_ids[0].id,
             duration: data.duration,
             aspect_ratio: data.aspect_ratio,
-            isReady: true
-          }
-        }
+            isReady: true,
+          },
+        },
       ];
     }
     case 'video.asset.errored': {
@@ -60,14 +59,15 @@ const processWebhookEvent = async (webhookEvent:any) => {
         muxAsset.id,
         {
           data: {
-            error_message: `${data.errors.type}: ${data.errors.messages[0] || ''}`
-          }
-        }
+            error_message: `${data.errors.type}: ${data.errors.messages[0] || ''}`,
+          },
+        },
       ];
     }
-    default: return undefined;
+    default:
+      return undefined;
   }
-}
+};
 
 // Do not go gentle into that good night,
 // Old age should burn and rave at close of day;
@@ -75,28 +75,25 @@ const processWebhookEvent = async (webhookEvent:any) => {
 const thumbnail = async (ctx: Context) => {
   const { playbackId } = ctx.params;
 
-  const response = await axios.get(
-    `https://image.mux.com/${playbackId}/thumbnail.png`,
-    {
-      params: ctx.query,
-      responseType: 'stream'
-    }
-  );
+  const response = await axios.get(`https://image.mux.com/${playbackId}/thumbnail.png`, {
+    params: ctx.query,
+    responseType: 'stream',
+  });
 
   ctx.response.set('content-type', response.headers['content-type']);
   ctx.body = response.data;
 };
 
-const submitDirectUpload = async (ctx:Context) => {
+const submitDirectUpload = async (ctx: Context) => {
   const { title, origin } = ctx.request.body;
 
-  const cors = origin || ctx.request.header.origin
+  const cors = origin || ctx.request.header.origin;
 
   const result = await getService('mux').getDirectUploadUrl(cors);
 
   const data = {
     title,
-    upload_id: result.id
+    upload_id: result.id,
   };
 
   await strapi.entityService.create(model, { data });
@@ -104,12 +101,12 @@ const submitDirectUpload = async (ctx:Context) => {
   ctx.send(result);
 };
 
-const submitRemoteUpload = async (ctx:Context) => {
+const submitRemoteUpload = async (ctx: Context) => {
   const { body } = ctx.request;
 
-  if(!body.url) {
-    ctx.badRequest("ValidationError", { errors: { "url": ["url cannot be empty"]}});
-    
+  if (!body.url) {
+    ctx.badRequest('ValidationError', { errors: { url: ['url cannot be empty'] } });
+
     return;
   }
 
@@ -118,7 +115,7 @@ const submitRemoteUpload = async (ctx:Context) => {
   const data = {
     asset_id: result.id,
     title: body.title,
-    url: body.url
+    url: body.url,
   };
 
   const response = await strapi.entityService.create(model, { data });
@@ -129,8 +126,8 @@ const submitRemoteUpload = async (ctx:Context) => {
 const deleteMuxAsset = async (ctx: Context) => {
   const { id, delete_on_mux } = ctx.request.body;
 
-  if(!id) {
-    ctx.badRequest("ValidationError", { errors: { "id": ["id needs to be defined"]}});
+  if (!id) {
+    ctx.badRequest('ValidationError', { errors: { id: ['id needs to be defined'] } });
 
     return;
   }
@@ -165,26 +162,29 @@ const deleteMuxAsset = async (ctx: Context) => {
   ctx.send(result);
 };
 
-const muxWebhookHandler = async (ctx:Context) => {
+const muxWebhookHandler = async (ctx: Context) => {
   const body = ctx.request.body;
   const sigHttpHeader = ctx.request.headers['mux-signature'];
 
   const config = await Config.getConfig('general');
 
-  if(sigHttpHeader === undefined || sigHttpHeader === '' || (Array.isArray(sigHttpHeader) && sigHttpHeader.length < 0)) {
+  if (
+    sigHttpHeader === undefined ||
+    sigHttpHeader === '' ||
+    (Array.isArray(sigHttpHeader) && sigHttpHeader.length < 0)
+  ) {
     ctx.throw(401, 'Webhook signature is missing');
   }
-  
-  if(Array.isArray(sigHttpHeader) && sigHttpHeader.length > 1) {
+
+  if (Array.isArray(sigHttpHeader) && sigHttpHeader.length > 1) {
     ctx.throw(401, 'we have an unexpected amount of signatures');
   }
 
   let sig;
 
-  if(Array.isArray(sigHttpHeader)){
+  if (Array.isArray(sigHttpHeader)) {
     sig = sigHttpHeader[0];
-  }
-  else{
+  } else {
     sig = sigHttpHeader;
   }
 
@@ -193,7 +193,7 @@ const muxWebhookHandler = async (ctx:Context) => {
   // Koa.js request (the middleware used for parsing requests).
 
   // let isSigValid;
-  
+
   // try {
   //   isSigValid = Webhooks.verifyHeader(JSON.stringify(body), sig, config.webhook_signing_secret);
   // } catch(err) {
@@ -219,5 +219,5 @@ export = {
   submitRemoteUpload,
   deleteMuxAsset,
   muxWebhookHandler,
-  thumbnail
+  thumbnail,
 };
