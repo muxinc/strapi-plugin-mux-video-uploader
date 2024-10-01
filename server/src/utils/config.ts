06a42a885639d2eb502c53954696589a2a370ffe
Erik Peña <erikpena@users.noreply.github.com>
2024-10-01 10:37:18 -0700

    Update to support file based config - Re-introduced mux-settings route and controller - Updated config references to use better naming

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,14 +1,15 @@
-import { CONFIG_NAME } from '../constants';
+import { PLUGIN_NAME } from '../constants';
 
 interface Config {
-  access_token?: string;
-  secret_key?: string;
-  playback_signing_secret?: string;
-  playback_signing_id?: string;
+  accessTokenId?: string;
+  secretKey?: string;
+  webhookSigningSecret?: string;
+  playbackSigningId?: string;
+  playbackSigningSecret?: string;
 }
 
 type GetConfigFunction = () => Promise<Config>;
 
-const getConfig: GetConfigFunction = async () => await strapi.config.get(`plugin::${CONFIG_NAME}`);
+const getConfig: GetConfigFunction = async () => await strapi.config.get(`plugin::${PLUGIN_NAME}`);
 
 export { getConfig };
