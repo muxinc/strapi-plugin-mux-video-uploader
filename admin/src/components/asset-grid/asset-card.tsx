fc1bcabe202173a9bee55a9431fccdccd8886e20
Erik Peña <erikpena@users.noreply.github.com>
2024-10-03 21:47:43 -0700

    Fix thumbnails - Updated thumbnail proxy to return correct content type header - Updated AssetCard to use url for image (not the image itself) - Updated AssetCard to wrap status icons in Flex for center-center - Updated AssetCard to use loading status for mux assets that do not have a playbackId - Updated AssetCard to cover image on cards

--- admin/src/components/asset-grid/asset-card.tsx
+++ admin/src/components/asset-grid/asset-card.tsx
@@ -50,7 +50,10 @@ interface Props {
 const AssetCard = (props: Props) => {
   const { muxAsset, onClick = () => {} } = props;
 
-  const [thumbnailImageUrl, setThumbnailImageUrl] = React.useState<string>('');
+  const [thumbnailImageUrl, setThumbnailImageUrl] = React.useState<string>(
+    // Empty pixel
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
+  );
 
   const { get } = useFetchClient();
   const { formatMessage, formatDate } = useIntl();
@@ -60,17 +63,11 @@ const AssetCard = (props: Props) => {
   const init = async (muxAsset: MuxAsset) => {
     const { playback_id } = muxAsset;
     if (muxAsset.playback_id !== null && muxAsset.signed) {
-      const { data: sigData } = await get(`${PLUGIN_ID}/sign/${playback_id}?type=thumbnail`);
-      const { data: imageData } = await get(`${PLUGIN_ID}/thumbnail/${playback_id}?token=${sigData.token}`);
+      const { data: sigData } = await get(`/${PLUGIN_ID}/sign/${playback_id}?type=thumbnail`);
 
-      setThumbnailImageUrl(imageData);
+      setThumbnailImageUrl(`/${PLUGIN_ID}/thumbnail/${playback_id}?token=${sigData.token}`);
     } else if (muxAsset.playback_id !== null) {
-      const { data: imageData } = await get(`${PLUGIN_ID}/thumbnail/${playback_id}`);
-      setThumbnailImageUrl(imageData);
-    } else {
-      setThumbnailImageUrl(
-        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
-      );
+      setThumbnailImageUrl(`/${PLUGIN_ID}/thumbnail/${playback_id}`);
     }
   };
 
@@ -80,10 +77,16 @@ const AssetCard = (props: Props) => {
 
   const renderCardAssetStatus = React.useCallback(() => {
     if (muxAsset.error_message !== null) {
-      return <WarningCircle color="danger500" />;
-    } else if (isLoading) {
       return (
-        <Loader small>{formatMessage({ id: getTranslation('AssetCard.loading'), defaultMessage: 'Loading' })}</Loader>
+        <Flex>
+          <WarningCircle color="danger500" />
+        </Flex>
+      );
+    } else if (isLoading || !muxAsset.playback_id) {
+      return (
+        <Flex>
+          <Loader small>{formatMessage({ id: getTranslation('AssetCard.loading'), defaultMessage: 'Loading' })}</Loader>
+        </Flex>
       );
     }
   }, [muxAsset]);
@@ -132,8 +135,8 @@ const AssetCard = (props: Props) => {
     <BoxStyled onClick={handleOnClick} title={errorTitle || loadingTitle || undefined}>
       <Card>
         <CardHeader>
-          <CardAsset src={thumbnailImageUrl}>
-            <Flex>{renderCardAssetStatus()}</Flex>
+          <CardAsset src={thumbnailImageUrl} style={{ objectFit: 'cover' }}>
+            {renderCardAssetStatus()}
           </CardAsset>
           {muxAsset.duration && <CardTimer>{secondsToFormattedString(muxAsset.duration)}</CardTimer>}
         </CardHeader>
