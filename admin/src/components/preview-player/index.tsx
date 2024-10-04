b73d5bdcde371d7f919e022b8a4ffa24ced158dd
Erik Peña <erikpena@users.noreply.github.com>
2024-10-03 22:05:07 -0700

    Updated preview player - Drop mux-videojs-kit as mux player now works again (yay) - Now using proxied thumbnail url for the manual poster of mux player (no longer inferred from playback-id) - Updated AssetCard to use the cool useSignedTokens hook for signed assets

--- admin/src/components/preview-player/index.tsx
+++ admin/src/components/preview-player/index.tsx
@@ -1,93 +1,58 @@
 import React from 'react';
-import { useFetchClient } from '@strapi/strapi/admin';
 import styled from 'styled-components';
 import MuxPlayer from '@mux/mux-player-react';
-// @ts-expect-error - No types provided
-import videojs from '@mux/videojs-kit';
 
 import { MuxAsset } from '../../../../server/src/content-types/mux-asset/types';
 import { useSignedTokens } from '../signed-tokens-provider';
 import pluginPkg from '../../../../package.json';
 import { PLUGIN_ID } from '../../pluginId';
 
-import '@mux/videojs-kit/dist/index.css';
-
 // THE FOLLOWING IS TEMPORARILY COMMENTED OUT DUE TO ISSUE WITH STRAPI
 // ts-expect-error styled-components typings are off
-// const MuxPlayerStyled = styled(MuxPlayer)`
-//   width: 100%;
-// `;
-
-// const PreviewPlayer = (props: { muxAsset?: MuxAsset }) => {
-//   const { muxAsset } = props;
-//   const tokens = useSignedTokens();
-
-//   if (!muxAsset?.playback_id || (muxAsset.signed && !tokens.video)) return null;
-
-//   return (
-//     <MuxPlayerStyled
-//       playbackId={muxAsset.playback_id}
-//       playback-token={tokens.video}
-//       thumbnail-token={tokens.thumbnail}
-//       storyboard-token={tokens.storyboard}
-//       metadata={{
-//         video_id: muxAsset.id,
-//         video_title: muxAsset.title,
-//         player_name: 'Strapi Admin Dashboard',
-//         player_version: pluginPkg.version,
-//         page_type: 'Preview Player',
-//       }}
-//       streamType="on-demand"
-//     />
-//   );
-// };
+const MuxPlayerStyled = styled(MuxPlayer)`
+  width: 100%;
+`;
 
 const PreviewPlayer = (props: { muxAsset?: MuxAsset }) => {
   const { muxAsset } = props;
 
-  const [posterUrl, setPosterUrl] = React.useState<string>();
+  const [posterUrl, setPosterUrl] = React.useState<string>(
+    // Empty pixel
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
+  );
 
-  const { get } = useFetchClient();
   const tokens = useSignedTokens();
 
-  React.useEffect(() => {
-    if (!muxAsset) return;
-
-    const player = videojs('mux-default', {
-      timelineHoverPreviews: false,
-      plugins: {
-        mux: {
-          data: {
-            beaconUrl: 'https://inferred.litix.io',
-            video_id: muxAsset.id,
-            video_title: muxAsset.title,
-            player_name: 'Strapi Admin Dashboard',
-            player_version: pluginPkg.version,
-            page_type: 'Preview Player',
-          },
-        },
-      },
-    });
-
-    player.src({ type: 'video/mux', src: muxAsset.playback_id });
-  }, []);
-
-  React.useEffect(() => {
-    if (!muxAsset?.playback_id || !tokens) return;
-
+  const init = async (muxAsset: MuxAsset) => {
     const { playback_id } = muxAsset;
+    if (muxAsset.playback_id !== null && muxAsset.signed) {
+      setPosterUrl(`/${PLUGIN_ID}/thumbnail/${playback_id}?token=${tokens.thumbnail}`);
+    } else if (muxAsset.playback_id !== null) {
+      setPosterUrl(`/${PLUGIN_ID}/thumbnail/${playback_id}`);
+    }
+  };
 
-    get(`${PLUGIN_ID}/thumbnail/${playback_id}`)
-    .then(result => {
-      const { data } = result;
-      setPosterUrl(tokens.thumbnail ? `${data}?token=${tokens.thumbnail}` : data);
-    });
-  }, [muxAsset, tokens]);
+  React.useEffect(() => {
+    muxAsset && init(muxAsset);
+  }, []);
 
   if (!muxAsset?.playback_id || (muxAsset.signed && !tokens.video)) return null;
 
   return (
-    <video id="mux-default" className="video-js vjs-16-9" controls preload="auto" width="100%" poster={posterUrl} />
+    <MuxPlayerStyled
+      playbackId={muxAsset.playback_id}
+      poster={posterUrl}
+      playback-token={tokens.video}
+      storyboard-token={tokens.storyboard}
+      metadata={{
+        video_id: muxAsset.id,
+        video_title: muxAsset.title,
+        player_name: 'Strapi Admin Dashboard',
+        player_version: pluginPkg.version,
+        page_type: 'Preview Player',
+      }}
+      streamType="on-demand"
+    />
   );
 };
 
