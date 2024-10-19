2bdca3ed3616163e17e3fb17fc55df46acc764c7
Erik Peña <erikpena@users.noreply.github.com>
2024-10-18 20:01:00 -0700

    Add config validation to plugin on startup

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,10 +1,28 @@
+import { Config } from '../utils/config';
+
 export default {
   default: {
-    accessToken: '',
+    accessTokenId: '',
     secretKey: '',
     webhookSigningSecret: '',
     playbackSigningId: '',
     playbackSigningSecret: '',
   },
-  validator() {},
+  validator(config: Config) {
+    const missingConfigs = [];
+
+    if (!config.accessTokenId) {
+      missingConfigs.push('accessTokenId');
+    }
+
+    if (!config.secretKey) {
+      missingConfigs.push('secretKey');
+    }
+
+    if (missingConfigs.length > 0) {
+      throw new Error(
+        `Please remember to set up the file based config for your plugin.  Refer to the "Configuration" of the README for this plugin for additional details.  Configs missing: ${missingConfigs.join(', ')}`
+      );
+    }
+  },
 };
